35829cc7ed4b15bceb000579784e5a60d0cfd9a4
icelynn <iceylynn1207@gmail.com>
2017-01-05 18:39:53 +0800

    Fix Scenes issues

--- Assets/Scripts/icelynn/TitleMenu.js
+++ Assets/Scripts/icelynn/TitleMenu.js
@@ -1,7 +1,26 @@
-﻿function Start() {
-	Application.LoadLevel(1);
+﻿
+// #pragma strict
+
+import UnityEngine.SceneManagement;
+
+function Start() {
+	SceneManager.LoadScene("frigidzone", LoadSceneMode.Single);
 }
 
 function Quit() {
-	Application.LoadLevel(3);
+	SceneManager.LoadScene("EndingQuit", LoadSceneMode.Single);
 }
+
+
+// public class TitleButtonClass extends MonoBehaviour {
+// 	function Start() {
+// 		SceneManager.LoadScene("frigidzone", LoadSceneMode.Additive);
+// 	}
+
+// 	function Quit() {
+// 		SceneManager.LoadScene("EndingQuit", LoadSceneMode.Additive);
+// 	}
+
+
+// }
+
